c1f36a36e4ad483b25b7942fa8e19b75990c6e81
pineapplefan1234yt <pineapplefanyt@gmail.com>
2020-11-23 16:52:57 +0000

    intents, gitignore, loading improved

--- bot.py
+++ bot.py
@@ -22,23 +22,34 @@ class c:
     Pink = '\033[95m'
     Cyan = '\033[96m'
 
+intents = discord.Intents.all()
+
 class Bot(commands.Bot):
     def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('t!', 'T!', 't1', 'T1'), **kwargs)
-        #super().__init__(command_prefix=commands.when_mentioned_or('m!', 'M!', 'm1', 'M1'), **kwargs)
+        #super().__init__(command_prefix=commands.when_mentioned_or('t!', 'T!', 't1', 'T1'), **kwargs)
+        super().__init__(command_prefix=commands.when_mentioned_or('m!', 'M!', 'm1', 'M1'), **kwargs)
 
         self.remove_command('help')
 
+        x = 0
+        m = len(config.cogs)
         for cog in config.cogs:
+            x += 1
             try:
+                print(f"{c.Cyan}[S] {c.CyanDark}Loading cog {x}/{m} ({cog})")
                 self.load_extension(cog)
             except Exception as exc:
-                print(f'{c.RedDark}[E] {c.Red}Could not load extension {cog} due to {exc.__class__.__name__}: {exc}{c.c}')
+                print(f'{c.RedDark}[E] {c.Red}Could not load cog {x} - extension {cog} due to {exc.__class__.__name__}: {exc}{c.c}')
 
     async def on_ready(self):
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="over your servers."), status=discord.Status.idle)
         print(f'{c.Cyan}[S] {c.CyanDark}Logged on as {self.user} (ID: {self.user.id}){c.c}')
 
-bot = Bot(owner_ids=[438733159748599813, 421698654189912064, 261900651230003201, 317731855317336067, 421698654189912064], case_insensitive=True, presence=None)
+bot = Bot(
+    owner_ids=[438733159748599813, 421698654189912064, 261900651230003201, 317731855317336067], 
+    case_insensitive=True, 
+    presence=None,
+    intents=intents
+)
 
 bot.run(config.token)
