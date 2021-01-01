6c4cde6a45bc401febafab9eacc0486f031a9d27
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-01-01 09:20:14 +0000

    1.2/2 --bug? 4

--- cogs/core.py
+++ cogs/core.py
@@ -250,10 +250,10 @@ class Core(commands.Cog):
         async with aiohttp.ClientSession() as session:
             async with session.post(
                     "https://api.clicksminuteper.net/validate",
-                    params=[
-                        ("code", code),
-                        ("ids", f"{ctx.guild.id}.{ctx.author.id}.{roleid}")
-                    ]
+                    data={
+                        "code": code,
+                        "ids": f"{ctx.guild.id}.{ctx.author.id}.{roleid}"
+                    }
                 ) as r:
                 if r:
                     await ctx.author.send(embed=discord.Embed(
