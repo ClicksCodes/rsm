a85fd4baa058b96ae87f61d845445464df124f7f
Pineapplefan <pineapplefanyt@gmail.com>
2021-01-27 17:37:39 +0000

    Update ping.py

--- cogs/ping.py
+++ cogs/ping.py
@@ -22,7 +22,7 @@ class Ping(commands.Cog):
 
         @app.route("/")
         async def ping():
-            return "200"
+            return self.bot.latency()
 
         self.bot.server_teardown = self._signal_handler
         task = await app.run_task(
