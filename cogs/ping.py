89c3d7a7c68977582ff582fca318636234399c82
pineapplefan1234yt <pineapplefanyt@gmail.com>
2021-02-20 15:03:02 +0000

    checking request

--- cogs/ping.py
+++ cogs/ping.py
@@ -43,7 +43,7 @@ class Ping(commands.Cog):
                         data={"code": code}
                     ) as r:
                         try:
-                            resp = await r.data
+                            resp = await r.body()
                             print(resp)
                         except Exception as e:
                             print(e)
