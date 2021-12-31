af4a7853cac66db2ee63c5ed66ecbb7ce0005719
Pineapplefan <pineapplefanyt@gmail.com>
2021-12-31 13:30:48 +0000

    ugh

--- cogs/commands/verify.py
+++ cogs/commands/verify.py
@@ -83,14 +83,13 @@ class Verify(commands.Cog):
         ).set_footer(text="Connecting"))
         async with aiohttp.ClientSession() as session:
             async with session.get("https://clicksminuteper.net") as r:
-                resp = await r.status
-        if resp != 200:
-            return await m.edit(embed=discord.Embed(
-                title=f"{self.emojis().icon.loading} Verify",
-                description=f"Our servers appear to be down, please contact the moderators "
-                            f"or try again later.",
-                colour=self.colours.red
-            ))
+                if r.status != 200:
+                    return await m.edit(embed=discord.Embed(
+                        title=f"{self.emojis().icon.loading} Verify",
+                        description=f"Our servers appear to be down, please contact the moderators "
+                                    f"or try again later.",
+                        colour=self.colours.red
+                    ))
         def _gencode(length=5):
             return [random.choice(
                 "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-_"
